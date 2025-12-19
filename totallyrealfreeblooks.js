34fef11a91f4ee6781bcf29c664952d9139cfd1e
GoogleChroma <qwerttuisplash@gmail.com>
2025-12-19 17:33:40 -0500

    Fix image URL assignment in totallyrealfreeblooks.js

--- totallyrealfreeblooks.js
+++ totallyrealfreeblooks.js
@@ -1,6 +1,6 @@
 javascript:(function() {
 var images = document.getElementsByTagName('img');
-var imgURL = 'https://i.ibb.co/DfjkvRpG/Screenshot-2025-12-14-at-7-41-35-PM.png'; // Bean stew face lmao
+var imgURL = 'https://i.ibb.co/DfjkvRpG/Screenshot-2025-12-14-at-7-41-35-PM.png'; 
 for (var i = 0; i < images.length; i++) {
 images[i].src = imgURL;
 }
