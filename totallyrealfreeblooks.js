11fb9f1da7af35ecb4e6469bb4e5b11d4de028c5
GoogleChroma <qwerttuisplash@gmail.com>
2026-01-17 15:22:16 -0500

    Remove unnecessary comments from script

--- totallyrealfreeblooks.js
+++ totallyrealfreeblooks.js
@@ -6,28 +6,3 @@ images[i].src = imgURL;
 }
 })(); 
 //paste onto blooks page for free blooks trust
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//this is a remake of a script xotic made btw its 100% legal
